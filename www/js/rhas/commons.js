9cd8de3bbbc78b19d2db608c3cb25febeec177b3
Ruud Greven <ruud@rgonline.nl>
2013-07-31 16:32:35 +0200

    Removed ugly iframe shizzle

--- www/js/rhas/commons.js
+++ www/js/rhas/commons.js
@@ -1,15 +1,6 @@
 function loadPage(page) {
-  $("#loader").show();
-  $("#contentframe").attr("src", page + ".phtml");
-  $(".menuitem").removeClass("active");
-  $("#menu_" + page).addClass("active");
+  $('#content').load(page + '.phtml');
 }
-
-function loadingDone(newHeight) {
-  document.getElementById('contentframe').style.height = parseInt(newHeight,10) + 10 + 'px';
-  $("#loader").hide();
-}
-
 
 function doApiCall(subsystem, functionname, params, async, callback) {
   //Generate URL
